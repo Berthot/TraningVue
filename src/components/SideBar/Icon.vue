dd1602eb6eb069702eedbb6a84c59682c3e8d792
berthot <matheus.berthot@gmail.com>
2021-04-27 00:33:58 -0300

    sidebar ts

--- src/components/SideBar/Icon.vue
+++ src/components/SideBar/Icon.vue
@@ -3,18 +3,24 @@
 </template>
 
 <script lang="ts">
-export default {
+import {defineComponent} from "vue";
+
+export default defineComponent({
   name: "Icon",
   props:{
-    iconName: String
+    iconName: {
+      type: String,
+      default: (): string => ""
+    }
   },
   computed:{
     iconClass(): string {
-      return `fas fa-${this.iconName}`
+      return "fas fa-" + this.iconName
     }
   }
-}
+})
 </script>
+
 
 <style scoped>
 
